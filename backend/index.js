1ac45a81d2b982594d1d13a2e0274c46c6d56e04
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-04-20 10:34:15 -0700

    setup database and connect to it

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB database.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.port, () => {
+  console.log(`Server is running on port ${process.env.port}`);
 });
+
+// username: abdulwaleedfsd
+// password: DNtXhtkeRDyJvrBV
+// connectionstring: mongodb+srv://abdulwaleedfsd:DNtXhtkeRDyJvrBV@hirehunar.pzuzff1.mongodb.net/hirehunar?retryWrites=true&w=majority&appName=hirehunar
+
+// list of commands for backend
+// npm init -y
+// npm i express
+// npm i mongoose
+// npm i dotenv
