b1e674b54e6dc8aa303d1f2f13de1fcc33bbc2cf
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-04-20 18:33:10 -0700

    create sign in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -35,3 +35,49 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { userType, email, password } = req.body;
+
+  if (
+    !userType ||
+    !email ||
+    !password ||
+    userType === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required!"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password!"));
+    }
+    if (validUser.userType !== req.body.userType) {
+      return next(errorHandler(400, "Invalid usertype!"));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+        userType: validUser.userType,
+        isAdmin: validUser.isAdmin,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
